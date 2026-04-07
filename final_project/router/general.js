ce82b987d67239ead8b1887008fdebf04d91fc2e
JG1795 <shido.galarza@gmail.com>
2026-04-07 00:09:02 -0400

    some changes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,33 +11,75 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
+//task 1
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Send JSON response with formatted books data
+  if (books) {
+     return res.status(200).send(JSON.stringify(books, null, 4));
+  } else {
+     return res.status(404).json({ message: "No books found" });
+  }
 });
 
 // Get book details based on ISBN
+//task 2
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Retrieve the isbn parameter from the request URL and send the corresponding book's details
+    const isbn = req.params.isbn;
+    if(books[isbn]){
+        return res.status(200).send(JSON.stringify(books[isbn],null,4));
+       }
+    else{
+        return res.status(404).send("No book found with ISBN "+isbn);
+    }
  });
   
 // Get book details based on author
+//task 3
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  let booksFound = [];
+  for (let isbn in books) {
+    if (books[isbn].author == author) {
+        booksFound.push(books[isbn]);
+    }
+  }
+  if (booksFound.length < 0) {
+    return res.status(200).send(JSON.stringify(booksFound,null,4));
+    }
+    else{
+       return res.status(404).send("No book found with author "+author);
+    }
 });
 
 // Get all books based on title
+//task 4
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    let title = req.params.title;
+    let booksFound = [];
+    for (let isbn in books) {
+      if (books[isbn].title == title) {
+          booksFound.push(books[isbn]);
+      }
+    }
+    if (booksFound.length < 0) {
+      return res.status(200).send(JSON.stringify(booksFound,null,4));
+      }
+      else{
+         return res.status(404).send("No book found with author "+title);
+      }
+  });
 
 //  Get book review
+//task 5
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    if(books[isbn]){
+      return res.status(200).send(JSON.stringify(books[isbn].reviews,null,4));
+    }
+    else{
+      return res.status(404).send("No book found with ISBN "+isbn);
+    }
 });
 
 module.exports.general = public_users;
